1e018d9d9b8dad3224817145c992947b6f19ab8f
Jack Penn <jacktpenn@gmail.com>
2025-07-01 21:43:10 -0400

    Rename secrets file for clarity

--- algorhythms-server/main.py
+++ algorhythms-server/main.py
@@ -3,14 +3,14 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-load_dotenv()
+load_dotenv(dotenv_path='./secrets.env')
 client_id = os.getenv("SPOTIFY_CLIENT_ID")
 
 print(client_id)
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="client_id",
-                                                           client_secret="YOUR_APP_CLIENT_SECRET"))
+# sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="client_id",
+#                                                            client_secret="YOUR_APP_CLIENT_SECRET"))
 
-results = sp.search(q='weezer', limit=20)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
+# results = sp.search(q='weezer', limit=20)
+# for idx, track in enumerate(results['tracks']['items']):
+#     print(idx, track['name'])
