962748f76ab9e282712101647952c3bbf771ffe7
Jack Penn <jacktpenn@gmail.com>
2025-07-04 01:14:07 -0400

    Add guest login

--- algorhythms-client/src/auth/ProtectedRoute.tsx
+++ algorhythms-client/src/auth/ProtectedRoute.tsx
@@ -2,8 +2,8 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
 
 const ProtectedRoute = () => {
-	const { token } = useAuth();
-	if (!token) return <Navigate to='/login' />;
+	const { user } = useAuth();
+	if (!user) return <Navigate to='/login' />;
 	return <Outlet />;
 };
 
---
+++ algorhythms-client/src/lib/types.ts
@@ -0,0 +1,18 @@
+import type { SpotifyUserProfile } from "./spotify-api";
+
+export type Weights = {
+	acousticness: number;
+	danceability: number;
+	energy: number;
+	instrumentalness: number;
+	liveness: number;
+	valence: number;
+	speechiness: number;
+	tempo: number;
+	loudness: number;
+};
+
+export type LoggedInUser = {
+	display_name: string;
+	spotify_profile: SpotifyUserProfile | null;
+};
