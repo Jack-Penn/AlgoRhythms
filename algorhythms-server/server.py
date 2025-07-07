efff0d71fe6ae2a8dc28ebca4e633ca5f4bf0ebb
Jack Penn <jacktpenn@gmail.com>
2025-07-06 21:08:58 -0400

    Outline generate playlist endpoint

--- algorhythms-server/server.py
+++ algorhythms-server/server.py
@@ -1,11 +1,12 @@
 import socket
 import threading
-from typing import Union
+from typing import Optional, Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import uvicorn 
-from gemini_api import generate_weights, generate_emoji
-from spotify_auth import handle_auth_callback, server_access
+from gemini_api import Weights, generate_weights, generate_emoji
+from spotify_auth import TokenInfo, get_access_from_user_token, handle_auth_callback, server_access
 from spotify_api import *
 
 # Run command: fastapi dev server.py
@@ -93,6 +94,43 @@ def server_auth_callback_endpoint(code: str, state: str):
         except Exception as e:
              print(f"Error: {str(e)}")
              return {"error": str(e)}
+
+class PlaylistRequest(BaseModel):
+    weights: Weights
+    auth: Optional[TokenInfo] = None
+@app.post("/create-playlist")
+async def create_playlist_endpoint(
+    request: PlaylistRequest,
+    mood: Union[str, None] = None,
+    activity: Union[str, None] = None,
+    length: Union[str, None] = None,
+    favorite_songs: Union[str, None] = None
+):
+    if(mood is None or activity is None or length is None):
+        return {
+            "error":"mood, activity, or length cannot be none"
+        }
+
+    # Split comma-separated favorite_songs into a list
+    favorite_songs_list: None | List[str] = None
+    if(favorite_songs is not None):
+        favorite_songs_list = favorite_songs.split(',')
+
+    spotify_access = algorhythms_account
+    if( request.auth is not None):
+        spotify_access = get_access_from_user_token(request.auth)
+
+    track_uris = ["spotify:track:04emojnbYkrRmv5qtJcgVP", "spotify:track:42UBPzRMh5yyz0EDPr6fr1", "spotify:track:2ipIPsgrgd0j2beDf4Ki70"]
+    playlist = create_playlist(spotify_access, "[Playlist name]", "[Playlist description]", track_uris)
+
+    if playlist is None:
+        return {
+            "error": "playlist wasn't created"
+        }
+
+    return {
+        "playlist_uri": playlist["uri"],
+    }
 
 # @app.get("/items/{item_id}")
 # async def read_item(item_id: int, q: Union[str, None] = None):
